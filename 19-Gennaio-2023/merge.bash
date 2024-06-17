2d485ae23f41dc52ea2e25f6c39a757bdfe4dd17
Lukirby <luca.panariello2@studio.unibo.it>
2024-06-17 11:28:18 +0200

    merge fatto

--- 19-Gennaio-2023/merge.bash
+++ 19-Gennaio-2023/merge.bash
@@ -9,11 +9,11 @@ dir2=$2
 #files1=($(find $dir1 | sed "s|^$dir1/||"))
 #files2=($(find $dir2 | sed "s|^$dir2/||"))
 
-files1=($(find $dir1 ))
-files2=($(find $dir2 ))
+dirlist1=($(find $dir1 -type d))
+dirlist2=($(find $dir2 -type d))
 
-echo "Files1: ${files1[@]}"
-echo "Files2: ${files2[@]}"
+echo "dirlist1: ${dirlist1[@]}"
+echo "dirlist2: ${dirlist2[@]}"
 
 merge="M"
 
@@ -21,37 +21,41 @@ mkdir -p $merge
 
 declare -i bool=0
 
-commonfiles=()
-alonefiles=()
-commondirs=()
-alonedirs=()
+files1=($(find $dir1 -type f ))
+files2=($(find $dir2 -type f ))
 
+echo "Files1 only Files: ${files1[@]}"
+echo "Files2 only Files: ${files2[@]}"
 
-for path1 in ${files1[@]}; do
-    
-    for path2 in ${files2[@]}; do
-        if [[ $path1 == $path2 && -d $ ]]; then
-            commondirs+=($path1)
-        fi
-    done
+for path1 in "${dirlist1[@]}"; do
+    path1=${path1#$dir1/}
+    if [[ -d $dir1/$path1 ]]; then
+        mkdir -p "$merge/$path1"
+    fi    
+done
+
+for path2 in "${dirlist2[@]}"; do
+    path2=${path2#$dir2/}
+    if [[ -d $dir2/$path2 && ! -d "$merge/$path2" ]]; then
+        path2=${path2#$dir2/}
+        mkdir -p "$merge/$path2"
+    fi    
 done
 
 for path2 in ${files2[@]}; do
-    if [[ $path2 != $dir2 ]]; then
-        path2=${path2#$dir2/}
-        bool=0
-        for path1 in ${files1[@]}; do
+    path2=${path2#$dir2/}
+    bool=0
+    for path1 in ${files1[@]}; do
             path1=${path1#$dir1/}
-            if [[ $path1 == $path2 && $path1 != $dir1 ]]; then
+            if [[ $path1 == $path2]]; then
                 cat $dir1/$path1 $dir2/$path2 > $merge/$path2
                 bool=1
                 break
             fi
-        done
-        if [[ $bool == 0 ]]; then
-            cp $dir2/$path2 $merge/$path2
-        else
-            bool=0
-        fi
+    done
+    if [[ $bool == 0 ]]; then
+        cp $dir2/$path2 $merge/$path2
+    else
+        bool=0
     fi
 done
