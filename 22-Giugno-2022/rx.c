49503a1231e6185271a00adb2e1d74d95d50da06
Luca Panariello <luca.panariello2@studio.unibo.it>
2024-06-21 12:34:53 +0200

    da risolvere

--- 22-Giugno-2022/rx.c
+++ 22-Giugno-2022/rx.c
@@ -13,9 +13,9 @@
 #define MAX_ARG_LEN 8
 
 void handler(int sig, siginfo_t *si, void *unused) {
-    printf("Signal number: %d\n", sig);
-    printf("Signal value: %d\n", si.s);
-    printf("%S",si->si_value.sival_ptr);
+    printf("Signal received\n");
+    printf("Signal value: %d\n", si->si_value.sival_int);
+    printf("%s",(char*)si->si_value.sival_ptr);
 }
 
 int main(int argc, char *argv[]) {
@@ -24,6 +24,7 @@ int main(int argc, char *argv[]) {
     char buffer[MAX_ARG_LEN];
 
     sa.sa_sigaction = handler;
+    sa.sa_flags = SA_SIGINFO;
     sigemptyset(&sa.sa_mask);
 
     if (sigaction(SIGUSR1, &sa, NULL) == -1) {
@@ -31,12 +32,13 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+
     printf("Pid: %d\n", getpid());
+
+    pause();
 
     // Rest of your code
 
     return 0;
-    
-
 }
 
--- 22-Giugno-2022/tx.c
+++ 22-Giugno-2022/tx.c
@@ -10,8 +10,8 @@
 #define MAX_ARG_LEN 8
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <fifo>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <fifo> string \n", argv[0]);
         exit(1);
     }
 
@@ -23,7 +23,9 @@ int main(int argc, char *argv[]) {
     union sigval value;
 
     value.sival_int = 42;
-    value.sival_ptr = buffer;
+    value.sival_ptr = (void*) buffer;
+
+   printf("Signal string: %s\n", value.sival_ptr);
 
     if (sigqueue(pid, SIGUSR1, value) == -1) {
         perror("kill");
